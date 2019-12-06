b7c652c66e2798aa52db26d062dc2c1dd6d37724
Kupu maru <kupumaru21@gmail.com>
2019-12-07 00:12:44 +0900

    # Conflicts: #	src/js/components/molecules/ArticleAuthor/index.vue

--- src/js/components/molecules/ArticleAuthor/index.vue
+++ src/js/components/molecules/ArticleAuthor/index.vue
@@ -45,7 +45,7 @@
                 underline
                 hover-opacity
               >
-                詳細(これbranchA用のコミット)(これbranchB用のコミット)
+                詳細(これbranchA用のコミット)
               </app-router-link>
             </td>
           </div>
